882ad0d3f281642d823ef66e1498a35e1c231cff
Chad Whiteley <chadwhiteley@apps.sheldonisd.com>
2017-06-25 05:14:48 +0000

    Adding email form.

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["Name"]; ?><br>
+Your email address is: <?php echo $_GET["Email"]; ?><br>
+Thank you for your input! <br>
+The message you sent to Mr. Whiteley is: <?php echo $_GET["Message"]; ?><br>
+He will get back with you ASAP.
+
+</body>
+</html>
